4c8e5b8f3457a7b7dfd0716c70038e9cdbcbb4b5
Javier Zapata <javierzapata82@gmail.com>
2020-04-03 11:45:49 +0200

    Updates

--- component---src-pages-404-js-d9414138fd8266899a69.js
+++ component---src-pages-404-js-d9414138fd8266899a69.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[7],{w2l6:function(n,t,e){"use strict";e.r(t);e("q1tI");var d=e("dH/O"),l=e("noox"),o=e("WsIH"),u=e("gZs9"),a=e("qKvR");t.default=function(){return Object(a.d)(d.a,null,Object(a.d)(o.a,null),Object(a.d)(l.a,null,Object(a.d)("div",{style:{marginTop:"100px"}},Object(a.d)(u.a.h1,null,"404: Page Not Found"))))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-b2b4dfbbe3848df72003.js.map
+//# sourceMappingURL=component---src-pages-404-js-d9414138fd8266899a69.js.map
